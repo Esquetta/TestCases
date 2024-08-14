886f64ec762c10d986b4ccedb002001aa288ed7e
Furkan <esquetta@gmail.com>
2024-08-14 13:45:03 +0300

    ReturnMissingOddNumber addedd

---
+++ ReturnMissingOddNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int ReturnMissingOddNumber(int[] nums)
+{
+	var sortedArray = nums.OrderDescending().ToArray();
+	for (int i = 0; i < sortedArray.Length-1; i++)
+	{
+		if (Math.Abs((sortedArray[i] - sortedArray[i + 1])) != 2)
+		{
+			return sortedArray[i] - 2;
+		}
+	}
+
+	return 0;
+
+}
+
+
+Console.WriteLine(ReturnMissingOddNumber(new int[] {1,3,5,7,9,13,15,17}));
+
+
