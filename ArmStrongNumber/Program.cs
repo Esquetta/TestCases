3ef33774d81b7df49f251b616eb2ff83e1a9e4bf
Furkan <esquetta@gmail.com>
2024-10-06 17:07:36 +0300

    Armstrong number test case added.

---
+++ ArmStrongNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+bool isArmsStrongNumber(int number)
+{
+    int numberOfDigits = number.ToString().Length;
+    int sum = 0;
+    int temp = number;
+
+    // Sayının her bir basamağını hesaplayıp uygun güçte topluyoruz.
+    while (temp > 0)
+    {
+        int digit = temp % 10;
+        sum += (int)Math.Pow(digit, numberOfDigits);
+        temp /= 10;
+    }
+
+    // Toplam sayıya eşit mi kontrolü yapıyoruz.
+    return sum == number;
+}
+
+Console.WriteLine(isArmsStrongNumber(15));
